1817bf08da0e7f22eb5e05c6b895188efce993d8
Owen Healy <owen@pulsetor.com>
2013-07-13 11:31:28 -0400

    Initial version

---
+++ quickstructures.py
@@ -0,0 +1,14 @@
+
+from collections import namedtuple as nt
+
+def adict(**things):
+    return things
+
+def struct(**things):
+    return nt('Struct', things.keys())(**things)
+
+def nstruct(name):
+    return struct(
+        of = lambda **things: nt(name, things.keys())(**things)
+    )
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name       = 'quickstructures',
+    version    = '0.123',
+    py_modules = ['quickstructures']
+)
+
